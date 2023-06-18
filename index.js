655421e783cbdab5a90e444fd54795a77ad9d958
KirillPurposeful <kirill.qa@megapartners.io>
2023-06-18 23:40:29 +0200

    What's the real floor?

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function positiveSum(arr) {
   }
   return sum;
 }
+//What's the real floor?
+if (n <= 0) {
+  return n;
+} else if (n === 1) {
+  return 0;
+} else if (n <= 12) {
+  return n - 1;
+} else {
+  return n - 2;
+}
