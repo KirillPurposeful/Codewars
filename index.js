6cb97fdb4d415bab0d1f82025dd3c078c47b8c47
KirillPurposeful <kirill.qa@megapartners.io>
2023-06-18 23:47:11 +0200

    Ninety Nine Thousand Nine Hundred Ninety Nine

--- index.js
+++ index.js
@@ -48,3 +48,4 @@ function DNAStrand(dna){
 
   return complementaryStrand;
 }
+
---
+++ index.ts
@@ -0,0 +1,43 @@
+//Ninety Nine Thousand Nine Hundred Ninety Nine
+export function numberToEnglish(x: number): string {
+    const ones: string[] = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const tens: string[] = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const teens: string[] = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const suffixes: string[] = ['', 'thousand', 'million', 'billion', 'trillion'];
+  
+    if (!Number.isInteger(x) || x < 0 || x > 99999) {
+      return '';
+    }
+  
+    if (x === 0) {
+      return 'zero';
+    }
+  
+    function convert(num: number): string {
+      if (num < 10) {
+        return ones[num];
+      } else if (num < 20) {
+        return teens[num - 10];
+      } else if (num < 100) {
+        return tens[Math.floor(num / 10)] + (num % 10 !== 0 ? ' ' + convert(num % 10) : '');
+      } else {
+        return ones[Math.floor(num / 100)] + ' hundred' + (num % 100 !== 0 ? ' ' + convert(num % 100) : '');
+      }
+    }
+  
+    const groups: number[] = [];
+    let num = x;
+    while (num > 0) {
+      groups.push(num % 1000);
+      num = Math.floor(num / 1000);
+    }
+  
+    let result: string = '';
+    for (let i = 0; i < groups.length; i++) {
+      if (groups[i] !== 0) {
+        result = convert(groups[i]) + ' ' + suffixes[i] + ' ' + result;
+      }
+    }
+  
+    return result.trim();
+  }
